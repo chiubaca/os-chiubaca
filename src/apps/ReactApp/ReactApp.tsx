a4334a94ac3c72cde42e18c050fecce523263f9b
Alex Chiu <alexchiu11@gmail.com>
2022-09-24 21:55:46 +0100

    initial commit 🌱

---
+++ src/apps/ReactApp/ReactApp.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ReactApp = () => {
+  let [counter, setCounter] = React.useState<number>(1);
+
+  return (
+    <>
+      <div>This is a react component</div>
+
+      {counter}
+
+      <button
+        className="border  border-gray-700"
+        onClick={() => {
+          setCounter(counter++);
+        }}
+      >
+        increment
+      </button>
+    </>
+  );
+};
+
+export default ReactApp;
