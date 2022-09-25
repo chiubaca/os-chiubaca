7e4a5204d52c2089aef8293bc2cd52c1c55fd1eb
Alex Chiu <alexchiu11@gmail.com>
2022-09-25 16:48:53 +0100

    feat: svelte example

---
+++ src/apps/svelte-counter/SvelteCounter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  function incrementCount() {
+    // event handler code goes here
+    count += 1;
+  }
+</script>
+
+<div>This a Svelte counter</div>
+<button
+  on:click={incrementCount}
+  class="g-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+>
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
+</button>
